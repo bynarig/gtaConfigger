02de53155be46dcc43162883e1cc31c404f1044f
Yaroslav Shabelnyk <62596807+bynarig@users.noreply.github.com>
2025-02-24 17:29:04 +0100

    pico clenup + axios migration

--- client/src/app/theme/index.ts
+++ client/src/app/theme/index.ts
@@ -1,5 +1,3 @@
-import './segmented/segmented.scss';
-import './slider/slider.scss';
 import './_variables.scss';
 import './index.scss';
 import './theme.scss';
--- client/src/entities/fileOptimisation.ts
+++ client/src/entities/fileOptimisation.ts
@@ -1,4 +1,6 @@
-export const sendSettingsToServer = async(PlayerDistanceSliderValue: number,VehicleDistanceSliderValue: number , TerrainDistanceSliderValue: number): Promise<any> => {
+import axios from 'axios';
+
+export const sendSettingsToServer = async (PlayerDistanceSliderValue: number, VehicleDistanceSliderValue: number, TerrainDistanceSliderValue: number): Promise<any> => {
   const settings = {
     settings: {
       loadDistances: {
@@ -10,20 +12,25 @@ export const sendSettingsToServer = async(PlayerDistanceSliderValue: number,Vehi
   };
 
   try {
-    const response = await fetch(`http://localhost:8905/optimisation/optimise-settings`, {
-      method: 'POST',
+    const response = await axios.post('http://localhost:8905/optimisation/optimise-settings', settings, {
       headers: {
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(settings),
-    });
+      }, responseType: 'blob',
+    })
+    // const response = await fetch(`http://localhost:8905/optimisation/optimise-settings`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(settings),
+    // });
 
-    if (response.ok) {
-      const blob = await response.blob();
+    if (response.status === 200) {
+      const blob = await response.data;
       const fileURL = URL.createObjectURL(blob);
-      return(fileURL);
+      return (fileURL);
     }
-      console.error('Server responded with an error');
+    console.error('Server responded with an error');
 
   } catch (error) {
     console.error('Error sending settings to server:', error);
